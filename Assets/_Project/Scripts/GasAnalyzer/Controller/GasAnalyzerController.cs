6d796775158ffb1a1334e3554f7fd098df63f9d0
Ima Laghima <imalaghima@gmail.com>
2025-09-27 20:51:46 +0500

    feat(GasAnalyzerController): make turn On and Off

--- Assets/_Project/Scripts/GasAnalyzer/Controller/GasAnalyzerController.cs
+++ Assets/_Project/Scripts/GasAnalyzer/Controller/GasAnalyzerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTask.Crane.InputProvider;
+using VRTask.GasAnalyzer.Common;
 using VRTask.GasAnalyzer.DangerZone;
 using VRTask.GasAnalyzer.Display;
 using VRTask.GasAnalyzer.Probe;
@@ -20,13 +22,56 @@ namespace VRTask.GasAnalyzer.Controller
         [SerializeField]
         private GasAnalyzerDisplay _display = null!;
 
+        [SerializeField]
+        private PowerButton _powerButton = null!;
+
+        [SerializeField]
+        private float _powerDelaySeconds = 3.0f;
+
+        private Coroutine? _powerCoroutine;
+        private Coroutine? _updateCoroutine;
+
 
         public bool IsLogging => _isLogging;
+        public GasAnalyzerState State { get; private set; }
+            = GasAnalyzerState.Inactive;
 
 
         private void Awake()
         {
             AssertInspectorRefsNotNull();
+        }
+
+        private void OnEnable()
+        {
+            _powerButton.OnPressed += PowerOn;
+            _powerButton.OnReleased += PowerOff;
+        }
+
+        private void OnDisable()
+        {
+            _powerButton.OnPressed -= PowerOn;
+            _powerButton.OnReleased -= PowerOff;
+        }
+
+
+
+        public void PowerOn()
+        {
+            if (_powerCoroutine == null)
+            {
+                Log(nameof(PowerOn) + " started");
+                _powerCoroutine = StartCoroutine(PowerCoroutine());
+            }
+        }
+
+        public void PowerOff()
+        {
+            if (_powerCoroutine != null)
+            {
+                StopCoroutine(_powerCoroutine);
+                Log(nameof(PowerOff) + " started");
+            }
         }
 
 
@@ -41,6 +86,11 @@ namespace VRTask.GasAnalyzer.Controller
                 _display != null,
                 "[GasAnalyzerController] Display reference is missing!"
             );
+
+            Debug.Assert(
+                _powerButton != null,
+                "[GasAnalyzerController] Power Button reference is missing!"
+            );
         }
 
         private void Log(string message)
@@ -51,6 +101,34 @@ namespace VRTask.GasAnalyzer.Controller
             }
         }
 
+        private void SwitchState()
+        {
+            if (State == GasAnalyzerState.Inactive)
+            {
+                State = GasAnalyzerState.Active;
+                _powerCoroutine = null;
+                _updateCoroutine = StartCoroutine(UpdateCoroutine());
+                // activate display
+                // activate probe
+                Log(State.ToString());
+            }
+            else if (State == GasAnalyzerState.Active)
+            {
+                State = GasAnalyzerState.Inactive;
+                _powerCoroutine = null;
+                StopCoroutine(_updateCoroutine);
+                // deactivate display
+                // deactivate probe
+                Log(State.ToString());
+            }
+        }
+
+
+        private IEnumerator PowerCoroutine()
+        {
+            yield return new WaitForSeconds(_powerDelaySeconds);
+            SwitchState();
+        }
 
         private IEnumerator UpdateCoroutine()
         {
