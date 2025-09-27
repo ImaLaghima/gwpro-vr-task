5fc525e40f7f75d1958bedc2afc49e0e0d3b510c
Ima Laghima <imalaghima@gmail.com>
2025-09-27 05:53:53 +0500

    feat: make CraneController move bean holder"
    Minor additions:
    - renamed object refs with 'Object' suffix
    - move asserts from Awake() into private helper
    
    Mixed actions. Not good commit.

--- Assets/_Project/Scripts/Crane/Controller/CraneController.cs
+++ Assets/_Project/Scripts/Crane/Controller/CraneController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using VRTask.Crane.RemoteController;
 
@@ -7,42 +9,37 @@ namespace VRTask.Crane.Controller
     public class CraneController : MonoBehaviour
     {
         [Header("Crane Input")]
+        [SerializeField]
         private CraneRemoteController _remoteController = null!;
 
-        [Header("Beam Holder")]
+        [Header("Beam Holder (BH)")]
         [SerializeField]
-        private GameObject _beamHolder = null!;
+        private GameObject _bhObject = null!;
+
+        [SerializeField]
+        private GameObject _bhMoveConstraintMin = null!;
+
+        [SerializeField]
+        private GameObject _bhMoveConstraintMax = null!;
+
+        [SerializeField]
+        private float _bhMoveSpeed = 3.7f;
 
         [Header("Beam")]
         [SerializeField]
-        private GameObject _beam = null!;
+        private GameObject _beamObject = null!;
 
         [Header("Hook")]
         [SerializeField]
-        private GameObject _craneHook = null!;
+        private GameObject _hookObject = null!;
+
+        private Coroutine? _moveCoroutine;
+        private Vector3 _moveDirection = Vector3.zero;
 
 
         private void Awake()
         {
-            Debug.Assert(
-                _remoteController != null,
-                "[CraneController] RemoteController reference is missing!"
-            );
-
-            Debug.Assert(
-                _beamHolder != null,
-                "[CraneController] BeamHolder reference is missing!"
-            );
-
-            Debug.Assert(
-                _beam != null,
-                "[CraneController] Beam reference is missing!"
-            );
-
-            Debug.Assert(
-                _craneHook != null,
-                "[CraneController] CraneHook reference is missing!"
-            );
+            AssertInspectorRefsNotNull();
         }
 
         private void OnEnable()
@@ -54,6 +51,8 @@ namespace VRTask.Crane.Controller
             _remoteController.OnNorth.AddListener(HandleNorth);
             _remoteController.OnSouth.AddListener(HandleSouth);
             _remoteController.OnActiveStopped.AddListener(HandleStop);
+
+            _moveCoroutine = StartCoroutine(MoveCoroutine());
         }
 
         private void OnDisable()
@@ -65,6 +64,8 @@ namespace VRTask.Crane.Controller
             _remoteController.OnNorth.RemoveListener(HandleNorth);
             _remoteController.OnSouth.RemoveListener(HandleSouth);
             _remoteController.OnActiveStopped.AddListener(HandleStop);
+
+            StopCoroutine(_moveCoroutine);
         }
 
 
@@ -90,17 +91,72 @@ namespace VRTask.Crane.Controller
 
         private void HandleNorth()
         {
-
+            _moveDirection = Vector3.forward;
         }
 
         private void HandleSouth()
         {
-
+            _moveDirection = Vector3.back;
         }
 
         private void HandleStop()
         {
+            _moveDirection = Vector3.zero;
+        }
 
+        private void AssertInspectorRefsNotNull()
+        {
+            Debug.Assert(
+                _remoteController != null,
+                "[CraneController] RemoteController reference is missing!"
+            );
+
+            Debug.Assert(
+                _bhObject != null,
+                "[CraneController] BeamHolder reference is missing!"
+            );
+
+            Debug.Assert(
+                _bhMoveConstraintMin != null,
+                "[CraneController] BeamHolder's MoveConstraintMin reference is missing!"
+            );
+
+            Debug.Assert(
+                _bhMoveConstraintMax != null,
+                "[CraneController] BeamHolder's MoveConstraintMax reference is missing!"
+            );
+
+            Debug.Assert(
+                _beamObject != null,
+                "[CraneController] Beam reference is missing!"
+            );
+
+            Debug.Assert(
+                _hookObject != null,
+                "[CraneController] CraneHook reference is missing!"
+            );
+        }
+
+
+        private IEnumerator MoveCoroutine()
+        {
+            // TODO: fix coroutine: prevent infinite work
+            while (true)
+            {
+                float nextPositionZ = _bhObject.transform.position.z +
+                                     (_bhMoveSpeed * Time.deltaTime * _moveDirection.z);
+                float clampedPositionZ = Mathf.Clamp(
+                    nextPositionZ,
+                    _bhMoveConstraintMin.transform.position.z,
+                    _bhMoveConstraintMax.transform.position.z
+                );
+                _bhObject.transform.position = new Vector3(
+                    _bhObject.transform.position.x,
+                    _bhObject.transform.position.y,
+                    clampedPositionZ
+                );
+                yield return null;
+            }
         }
     }
 }
